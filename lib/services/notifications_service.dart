8d450c6454d65b6b3a31b2f97a8dea4302d89193
Δημήτρης Μήλιος <damilios@protonmail.com>
2025-03-26 12:46:49 +0200

    Update notifications service

--- lib/services/notifications_service.dart
+++ lib/services/notifications_service.dart
@@ -11,12 +11,18 @@ class NotificationService {
   final _messaging = FirebaseMessaging.instance;
 
   Future<void> initialize() async {
-    await _messaging.requestPermission();
-    if (Platform.isIOS) {
-      final apnsToken = await _messaging.getAPNSToken();
-      log('FCM Token: $apnsToken');
+    NotificationSettings settings = await _messaging.requestPermission();
+    if(settings.authorizationStatus == AuthorizationStatus.authorized){
+      if (Platform.isIOS) {
+        final apnsToken = await _messaging.getAPNSToken();
+        log('FCM Token: $apnsToken');
+      }
+      final token = await _messaging.getToken();
+      log('FCM Token: $token');
+    } else if (settings.authorizationStatus == AuthorizationStatus.denied) {
+      log("❌ User denied notifications.");
+    } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+      log("⚠️ User granted provisional permissions (iOS only).");
     }
-    final token = await _messaging.getToken();
-    log('FCM Token: $token');
   }
 }
